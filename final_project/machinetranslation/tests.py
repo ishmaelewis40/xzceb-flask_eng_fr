661cc76fb46d61f5fa9ad5f05e107c7ea12ba6d2
ishmaelewis40 <ishmaelewis40@gmail.com>
2023-05-29 22:04:10 -0400

    finalchanges

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -6,7 +6,8 @@ from translator import french_to_english
 class TestEnglishToFrench(unittest.TestCase):
 
     def test_english_to_french(self):
-        self.assertNotEqual(english_to_french(''), '')
+        self.assertNotEqual(english_to_french("None"), '')
+        self.assertNotEqual(english_to_french(0), 0)
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
         self.assertNotEqual(english_to_french('Hello'), 'Hello')
 
@@ -14,8 +15,10 @@ class TestEnglishToFrench(unittest.TestCase):
     def test_french_to_english_translate(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
         self.assertNotEqual(french_to_english('bonjour'), 'bonjour')
-        self.assertNotEqual(french_to_english(''), '')
+        self.assertNotEqual(french_to_english("None"), '')
+        self.assertNotEqual(french_to_english(0), 0)
 
+       
    
 
 if __name__ == '__main__':
--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -5,7 +5,8 @@ from translator import french_to_english
 class TestEnglishToFrench(unittest.TestCase):
 
     def test_english_to_french(self):
-        self.assertEqual(english_to_french(None), None)
+        self.assertNotEqual(english_to_french("None"), '')
+        self.assertNotEqual(english_to_french(0), 0)
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
         self.assertNotEqual(english_to_french('Hello'), 'Hello')
 
@@ -13,7 +14,8 @@ class TestEnglishToFrench(unittest.TestCase):
     def test_french_to_english_translate(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
         self.assertNotEqual(french_to_english('bonjour'), 'bonjour')
-        self.assertEqual(french_to_english(None), None)
+        self.assertNotEqual(french_to_english("None"), '')
+        self.assertNotEqual(french_to_english(0), 0)
 
    
 
